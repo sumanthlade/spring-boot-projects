1be05751091611267200949aaa058a5546345dbb
Sumanth Lade <ladesaisumanth2000@gmail.com>
2024-03-09 02:15:54 -0600

    Hello World

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+/*
+ * <dependencies> <!-- Spring Web --> <dependency>
+ * <groupId>org.springframework</groupId> <artifactId>spring-web</artifactId>
+ * <version>5.2.8.RELEASE</version> </dependency>
+ * 
+ * <!-- Spring Web MVC --> <dependency> <groupId>org.springframework</groupId>
+ * <artifactId>spring-webmvc</artifactId> <version>5.2.8.RELEASE</version>
+ * </dependency> </dependencies>
+ */
